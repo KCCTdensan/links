ae729a6208c9b29944ff85ea279ff72a978bec02
YamaD <me@dyama.net>
2021-12-18 00:59:45 +0900

    てすと

---
+++ gen.d
@@ -0,0 +1,27 @@
+import std;
+
+immutable targets=[
+  "301",
+  "302",
+];
+immutable ext=".txt";
+immutable dir="dist";
+immutable file="_redirects";
+
+void main() {
+  if(!dir.exists) dir.mkdir;
+  auto f=File(dir~"/"~file, "w");
+  foreach(target; targets) {
+    auto path=target~ext;
+    if(!path.exists) continue;
+    auto rules=path.readText
+      .splitter(regex("\n"))
+      .map!strip
+      .filter!`a!=""&&a[0]!='#'`;
+    foreach(rule; rules) {
+      auto row=rule.split(regex(r"\s+"));
+      if(row.length!=2) continue;
+      f.writefln("%s %s "~target, row[0], row[1]);
+    }
+  }
+}
